20bc7ecbc29d2ca60fa574fff2b09fba89cd1de2
opportunato <ryu.gordeyev@gmail.com>
2015-02-14 23:28:32 +0300

    Fixed videos layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  def video_group(videos)
-    videos.each_with_index.reduce({first: [], second: []}) do |memo, (video, index)|
-      if index == 0
-        memo[:main] = video
-      elsif index % 2 == 1
-        memo[:first].push(video)
-      else
-        memo[:second].push(video)
-      end
-
-      memo
-    end
-  end
-
   def format_date(date)
     date.strftime("%d.%m.%Y")
   end
