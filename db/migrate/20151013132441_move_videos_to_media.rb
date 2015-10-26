091501be182e00b72e80847410572f1dd2a01d72
opportunato <ryu.gordeyev@gmail.com>
2015-10-26 19:40:18 +0300

    disabled unnecessary migration

--- db/migrate/20151013132441_move_videos_to_media.rb
+++ db/migrate/20151013132441_move_videos_to_media.rb
@@ -1,26 +1,26 @@
 class MoveVideosToMedia < ActiveRecord::Migration
   def change
-    Brand.transaction do
-      Tagging.all.update_all("taggable_type = 'Brand'")
+    # Brand.transaction do
+    #   Tagging.all.update_all("taggable_type = 'Brand'")
 
-      Brand.all.each do |brand|
-        medium = Medium.new(
-          name:         brand.name,
-          director:     brand.director,
-          music:        brand.music,
-          vimeo_id:     brand.vimeo_id,
-          link:         brand.link,
-          description:  brand.description,
-          is_published: brand.is_published
-        )
-        medium.thumbnail = File.new(File.join(Rails.root, '/public' + brand.thumbnail.url))
-        medium.save!
-        medium.thumbnail.recreate_versions!
-        brand.media << medium
-        brand.name = brand.brand
-        brand.save!
-        brand.tags
-      end
-    end
+    #   Brand.all.each do |brand|
+    #     medium = Medium.new(
+    #       name:         brand.name,
+    #       director:     brand.director,
+    #       music:        brand.music,
+    #       vimeo_id:     brand.vimeo_id,
+    #       link:         brand.link,
+    #       description:  brand.description,
+    #       is_published: brand.is_published
+    #     )
+    #     medium.thumbnail = File.new(File.join(Rails.root, '/public' + brand.thumbnail.url))
+    #     medium.save!
+    #     medium.thumbnail.recreate_versions!
+    #     brand.media << medium
+    #     brand.name = brand.brand
+    #     brand.save!
+    #     brand.tags
+    #   end
+    # end
   end
 end
