83e39e86ef69d93b41a3e1c225c67555e12a51b2
opportunato <ryu.gordeyev@gmail.com>
2015-10-26 19:26:06 +0300

    fixed possible brand without media bug

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   skip_before_filter :require_login
 
   def index
-    @brands = Brand.published
+    @brands = Brand.published.select { |brand| brand.media.length > 0 }
     @news   = News.published
   end
 end
