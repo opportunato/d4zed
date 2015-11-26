3d7e55679076b9e7887e1bbbaaa1e1dde5e7e8c8
opportunato <ryu.gordeyev@gmail.com>
2015-11-26 11:22:38 +0300

    fixed missing published scope on brands media

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -4,5 +4,7 @@ class Medium < ActiveRecord::Base
   belongs_to :brand
   has_many :credits, -> { order(order_number: :desc) }, dependent: :destroy
 
+  scope :published, -> { where(is_published: true).order('order_number DESC, created_at DESC') }
+
   accepts_nested_attributes_for :credits, allow_destroy: true
 end
